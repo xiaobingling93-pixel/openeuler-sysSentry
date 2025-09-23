9558b9bc7ad4956d4a83a2ba4d4928ae3b72e079
Xiaofei Tan <tanxiaofei@huawei.com>
2025-09-23 11:58:03 +0800

    sysSentry: Add MulanV2 License statement
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/sysSentry/issues/ICV952
    CVE: NA
    
    ----------------------------------------------------------------------
    
    Add MulanV2 license statement to the header of each file.
    
    Signed-off-by: Xiaofei Tan <tanxiaofei@huawei.com>
    Signed-off-by: Qizhi Zhang <zhangqizhi3@h-partners.com>

--- src/sentryPlugins/soc_ring_sentry/tc_ring_one.c
+++ src/sentryPlugins/soc_ring_sentry/tc_ring_one.c
@@ -1,5 +1,15 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * sysSentry is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ *
  * Description: tc ring testcase program
  * Author: lizixian
  * Create: 2025-7-10
