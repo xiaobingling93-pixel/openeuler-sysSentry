0232561cbeb5ae0744ab36c8658ad8c8478d9d07
shixuantong <shixuantong1@huawei.com>
2026-02-03 14:22:32 +0800

    fix potential memory leak issues in sentry_msg_monitor

--- src/sentryPlugins/sentry_msg_monitor/sentry_msg_monitor.c
+++ src/sentryPlugins/sentry_msg_monitor/sentry_msg_monitor.c
@@ -45,8 +45,14 @@
 #define PYHS_ADDR_HEX_STR_MAX_LEN 20
 
 struct receiver_cleanup_data {
+    int fd;
     struct alarm_msg *al_msg;
     struct alarm_register* register_info;
+};
+
+struct sender_cleanup_data {
+    int fd;
+    char *str;
 };
 
 static int handle_file_lock(int fd, bool lock)
@@ -371,9 +377,12 @@ static unsigned short convert_msg_type_to_xalarm_type(enum sentry_msg_helper_msg
 static void sender_cleanup(void* arg)
 {
     logging_debug("In sender thread cleanup\n");
-    int fd = *(int *)arg;
-    if (fd > 0) {
-        close(fd);
+    struct sender_cleanup_data *scd = (struct sender_cleanup_data *) arg;
+    if (scd->fd > 0) {
+        close(scd->fd);
+    }
+    if (scd->str) {
+        free(scd->str);
     }
     logging_info("Sender thread cleanup over\n");
 }
@@ -385,7 +394,6 @@ static void* sender_thread(void* arg)
     if (fd < 0) {
         goto close_recv;
     }
-    pthread_cleanup_push(sender_cleanup, &fd);
 
     pthread_t partner_t;
     char *str = (char *) calloc (MSG_STR_MAX_LEN, sizeof(char));
@@ -394,6 +402,12 @@ static void* sender_thread(void* arg)
         close(fd);
         goto close_recv;
     }
+
+    struct sender_cleanup_data scd = {
+        .fd = fd,
+        .str = str,
+    };
+    pthread_cleanup_push(sender_cleanup, &scd);
 
     while (1) {
         struct sentry_msg_helper_msg smh_msg;
@@ -467,6 +481,9 @@ static void receiver_cleanup(void* arg)
 {
     logging_debug("In receiver thread cleanup\n");
     struct receiver_cleanup_data* rcd = (struct receiver_cleanup_data*) arg;
+    if (rcd->fd > 0) {
+        close(rcd->fd);
+    }
     if (rcd->al_msg) {
         free(rcd->al_msg);
     }
@@ -527,6 +544,7 @@ re_register:
     }
 
     struct receiver_cleanup_data rcd = {
+        .fd = fd,
         .al_msg = al_msg,
         .register_info = register_info
     };
